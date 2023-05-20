2649eed67ecdc0caf7961179eb2e3728d60f039a
Houston Barros <capar.2022113TADS0170@aluno.ifpi.edu.br>
2023-05-19 22:16:42 -0300

    Criação das Rotas

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,22 +1,22 @@
-"""
-URL configuration for biblioteca project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from usuarios.api.viewsets import UsuarioViewSet
+from livros.api.viewsets import LivroViewSet
+from emprestimos.api.viewsets import EmprestimoViewSet, EmprestimoPorUsuarioAPIView, EmprestimoEmAtraso, EmprestimoConsulta
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
     path('admin/', admin.site.urls),
+    path('livros/', LivroViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('livros/<int:pk>/', LivroViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+    path('usuarios/', UsuarioViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('usuarios/<int:pk>/', UsuarioViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+    path('emprestimos/', EmprestimoViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('emprestimos/<int:pk>/', EmprestimoViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+    path('emprestimos/usuario/<int:usuario_id>/', EmprestimoPorUsuarioAPIView.as_view()),
+    path('emprestimos/atraso/<int:usuario_id>/', EmprestimoEmAtraso.as_view()),
+    path('emprestimos/consulta/', EmprestimoConsulta.as_view())
 ]
