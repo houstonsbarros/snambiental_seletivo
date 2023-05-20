db59986fd662e9f81d09cb35ea6d56244abecde1
Houston Barros <capar.2022113TADS0170@aluno.ifpi.edu.br>
2023-05-19 22:53:35 -0300

    Criação de Fixtures

--- database.sql
+++ database.sql
@@ -30,25 +30,3 @@ create table emprestimos_emprestimo (
     foreign key (livro_id) references livros_livro(id),
     foreign key (usuario_id) references usuarios_usuario(id)
 );
-insert into livros_livro(vTitulo, vAutor, iAno_publicacao, vEditora) values
-('Dom Quixote', 'Miguel de Cervantes', 1605, 'Editora'),
-('1984', 'George Orwell', 1949, 'Editora'),
-('O Grande Gatsby', 'F. Scott Fitzgerald', 1925, 'Editora'),
-('Orgulho e Preconceito', 'Jane Austen', 1813, 'Editora'),
-('Cem Anos de Solidão', 'Gabriel García Márquez', 1967, 'Editora'),
-('O Apanhador no Campo de Centeio', 'J.D. Salinger', 1951, 'Editora'),
-('Crime e Castigo', 'Fiódor Dostoiévski', 1866, 'Editora'),
-('Ulisses', 'James Joyce', 1922, 'Editora'),
-('Em Busca do Tempo Perdido', 'Marcel Proust', 1913, 'Editora'),
-('Moby Dick', 'Herman Melville', 1851, 'Editora');
-
-insert into usuarios_usuario(vNome, dNascimento, vCpf, vEmail) values
-('Elias Pietro Davi Almada', '2003-05-01', '99854163334', 'elias_almada@gmail.com'),
-('Emilly Marlene Vanessa Fernandes', '1995-03-15', '95894876370', 'emilly_marlene_fernandes@gmail.com'),
-('Débora Beatriz da Conceição', '1963-03-20', '14609619350', 'debora.beatriz.daconceicao@gmail.com'),
-('Geraldo Carlos Eduardo André Aragão', '1944-04-20', '90292267304', 'geraldo-aragao73@gmail.com'),
-('Danilo Lucas Carlos Almeida', '1978-04-22', '72426968362', 'danilo-almeida94@gmail.com');
-
-insert into emprestimos_emprestimo(livro_id, usuario_id, dData_emprestimo, dData_devolucao) values
-(1, 1, '2023-05-09', '2023-05-15'),
-(2, 2, '2023-05-13', '2023-05-20');
